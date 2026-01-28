95ae6693a7be62a54e2311a451611d5ce169dede
JuanECC <gatospro2000@gmai.com>
2026-01-27 19:54:15 -0600

    Primer commit

---
+++ dia1/main.js
@@ -0,0 +1,64 @@
+//Este es un comentario en JavaScript
+
+// Tipos de datos
+
+// Numer: enteros y decimales
+
+let edad = 24;
+let precio= 199.99;
+console.log("number");
+console.log(edad);
+console.log(precio);
+console.log(typeof edad);
+console.log(typeof precio);
+
+
+// String: cadenas de texto
+
+let nombre = "Juan";
+let saludo = "hola mundo";
+console.log("Tipo de dato string");
+console.log(nombre);
+console.log(saludo);
+console.log(typeof nombre);
+console.log(typeof saludo);
+
+// Boolean: true o false
+let esMayordeEdad = true;
+let TienePErmiso = false;
+console.log("Tipo de dato boolean");
+console.log(esMayordeEdad);
+console.log(TienePErmiso);
+console.log(typeof esMayordeEdad);
+
+//undefined: una variable declarada sin valor
+let telefono;
+console.log("Tipo de dato undefined");
+console.log(telefono);
+console.log(typeof telefono);
+
+// Null (Valor intenci0onalmente vacio)
+console.log("Tipo de dato null");
+let direccion = null;
+console.log(direccion);
+console.log(typeof direccion);
+
+// Sale object por que es un error historico que no fue corregido
+
+// Object (Estructura de datos con propiedades)
+
+let persona = {
+    nombre: "Juan",
+    edad: 20,
+    ciudad: "CDMX"
+};
+console.log("Tipo de dato object");
+console.log(typeof persona);
+console.log(persona);
+
+// Symbol (Identificador unico )
+let id = Symbol("id");
+console.log("Tipo de dato symbol");
+console.log(id);
+console.log(typeof id);
+
