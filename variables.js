a54cf57569cd3611180cbaa2168dbe78c937d87a
JuanECC <gatospro2000@gmai.com>
2026-01-29 19:32:46 -0600

    feat: variables

---
+++ variables.js
@@ -0,0 +1,62 @@
+let mascota = "Perro";
+let mascota1 ="Gato";
+
+const numero = 23;
+
+let num1 = 10;
+let num2 = 5;
+
+let resta = num1 - num2;
+let suma = num1 + num2;
+
+let modulo = num1 % num2;
+
+let exponencioal = num1 ** num2;
+
+console.log(suma);
+console.log(15 + 15);
+console.log(resta);
+console.log(num1*num2);
+console.log(num1/num2);
+
+
+console.log("modulo de 3 % 5 " + modulo);
+
+console.log(exponencioal);
+
+//Comparaciones simples
+console.log(num1== num2);
+console.log(10 == 10);
+console.log(12 == "12");
+
+//Comparacion estricta, triple igual
+console.log(23 === "23");
+
+//Mayor que
+let valor = 20;
+let valor2 = 30;
+
+//Mayor que 
+console.log(valor > valor2);
+
+//Mayor o igual que
+console.log(valor >= valor2);
+
+//Menor que 
+console.log(valor < valor2);
+
+//Menor o igual que
+console.log(valor <= valor2);
+
+//Distinto que
+console.log(10 != "10");
+
+//Distinto estricto que
+console.log(10 !== "10");
+
+
+console.log(valor + 2);
+
+valor = valor + 5;
+
+console.log(valor);
