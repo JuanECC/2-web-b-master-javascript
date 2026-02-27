27a1c340310d84f1a98d1aba8a8d6deae90ab848
JuanECC <gatospro2000@gmai.com>
2026-02-27 10:47:57 -0600

    Ejemplo de DOM lista de super

--- main.js
+++ main.js
@@ -1,70 +1,33 @@
-//Este es un comentario en JavaScript
+//Paso 1) Seleccionar mis elementos que voy a ocupar
 
-// Tipos de datos
+let itemInput = document.getElementById("itemInput");
+let addItemButton = document.getElementById('addItemButton');
+let itemList = document.getElementById("itemList");
 
-// Numer: enteros y decimales
+//Paso 2) Crear mi funcion 
+function addItemButtonClik(){
+    let itemText = itemInput.value;
+    
+    let newItem = document.createElement('li');
 
-let edad = 24;
-let precio= 199.99;
-console.log("number");
-console.log(edad);
-console.log(precio);
-console.log(typeof edad);
-console.log(typeof precio);
+    //Crear un elemento span 
+    let textSpan = document.createElement('span');
+    textSpan.textContent = itemText;
+    newItem.appendChild(textSpan)
 
+    //Crear el boton eliminar 
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Eliminar'
 
-// String: cadenas de texto
+    
+    deleteButton.addEventListener('click', function(){
+        newItem.remove();
+    })
 
-let nombre = "Juan";
-let saludo = "hola mundo";
-console.log("Tipo de dato string");
-console.log(nombre);
-console.log(saludo);
-console.log(typeof nombre);
-console.log(typeof saludo);
+    newItem.appendChild(deleteButton)
+    itemList.appendChild(newItem)
+    itemInput.value = '';
+}
 
-// Boolean: true o false
-let esMayordeEdad = true;
-let TienePErmiso = false;
-console.log("Tipo de dato boolean");
-console.log(esMayordeEdad);
-console.log(TienePErmiso);
-console.log(typeof esMayordeEdad);
-
-//undefined: una variable declarada sin valor
-let telefono;
-console.log("Tipo de dato undefined");
-console.log(telefono);
-console.log(typeof telefono);
-
-// Null (Valor intenci0onalmente vacio)
-console.log("Tipo de dato null");
-let direccion = null;
-console.log(direccion);
-console.log(typeof direccion);
-
-// Sale object por que es un error historico que no fue corregido
-
-// Object (Estructura de datos con propiedades)
-
-let persona = {
-    nombre: "Juan",
-    edad: 20,
-    ciudad: "CDMX"
-};
-console.log("Tipo de dato object");
-console.log(typeof persona);
-console.log(persona);
-
-// Symbol (Identificador unico )
-let id = Symbol("id");
-console.log("Tipo de dato symbol");
-console.log(id);
-console.log(typeof id);
-
-/* Se pone; al final de cada linea de codigo
-Por buenas practicas y evitar errores */
-
-let a = "pera";
-let fruta = "mango"; //  Usar nombres descrioptivos en las variables
-
+// Agregamos el evento escuchador click y le asignamos nuestra funcion que creamos 
+addItemButton.addEventListener('click', addItemButtonClik);
